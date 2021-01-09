a990ab16232af3afdb227fa72ad4b0e6cb08be76
EdwardGa0 <edwardgaobot@gmail.com>
2021-01-08 21:03:39 -0500

    homepage styling

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Fade, Jumbotron, Container, Row, Col } from "react-bootstrap";
 import Background from "./images/home_bg.jpg";
-import clock from "./images/clock.jpg"
+import clock from "./images/clock.png";
+import search from "./images/search.png";
+import friends from "./images/friends.png";
 
 const jumboStyle = {
   minHeight: "40vw",
@@ -15,40 +17,64 @@ const titleFont = {
   fontSize: "80px",
 };
 
+const imgStyle = {
+  height: "200px",
+  width: "200px",
+  display: "block",
+  margin: "0 auto",
+  backgroundColor: "transparent",
+};
+
 export default function HomePage() {
   return (
     <>
       <Jumbotron fluid style={jumboStyle}>
         <Fade in={true}>
-          <h1 className="text-light text-center user-select-none" style={titleFont}>
+          <h1
+            className="text-light text-center user-select-none"
+            style={titleFont}
+          >
             Progress at your Fingertips
           </h1>
         </Fade>
       </Jumbotron>
-      <Container>
+      <Container className="mb-5">
         <Row>
-          <Col>
-            <img src={clock} alt="clock"></img>
-            <h2 className="text-center">Practice</h2>
-            <p className="text-center">
-              Log your practice and keep track of your improvement.
-            </p>
+          <Col className="mr-6">
+            <img src={clock} style={imgStyle} alt="clock"></img>
+            <div className="w-75 " style={{ margin: "0 auto" }}>
+              <h2 className="text-center">Practice</h2>
+              <p className="text-center">
+                Log your practice and keep track of your improvement.
+              </p>
+            </div>
+          </Col>
+          <Col className="">
+            <img src={search} style={imgStyle} alt="search"></img>
+            <div className="w-75 " style={{ margin: "0 auto" }}>
+              <h2 className="text-center">Explore</h2>
+              <p className="text-center">
+                Learn new songs that suit your skill and taste.
+              </p>
+            </div>
           </Col>
           <Col>
-            <h2 className="text-center">Explore</h2>
-            <p className="text-center">
-              Learn new songs that suit your skill and taste.
-            </p>
-          </Col>
-          <Col>
-            <h2 className="text-center">Community</h2>
-            <p className="text-center">
-              Connect with others to share your favorite tunes.
-            </p>
+            <img src={friends} style={imgStyle} alt="search"></img>
+            <div className="w-75 " style={{ margin: "0 auto" }}>
+              <h2 className="text-center">Community</h2>
+              <p className="text-center">
+                Connect with others to share your favorite tunes.
+              </p>
+            </div>
           </Col>
         </Row>
       </Container>
-      <hr className="featurette-divider"></hr>
+      <Jumbotron className="bg-dark">
+        <h1 className="text-light text-center user-select-none">
+          Sign up to get started
+        </h1>
+      </Jumbotron>
+      
     </>
   );
 }
